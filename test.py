2341528a1309dc1c5806884948a716d28a1a14f3
LCZforever <794295908@qq.com>
2018-09-05 20:30:23 +0800

    add a simple wed spider

---
+++ test.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.error imp
+
+def getTitle(url):
+	try:
+		html=urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bsObj=BeautifulSoup(html.read())
+		title=bsObj.body.h1
+	except AttributeError as e:
+		return None
+	return title
+
+def getName(url):
+	try:
+		html=urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bsObj=BeautifulSoup(html)
+		namelist=bsObj.findAll("span",{"class":"green"})
+	except AttributeError as e:
+		return None
+	return namelist
+	
+								
+url="http://www.pythonscraping.com/pages/page1.html"
+url2="http://www.pythonscraping.com/pages/warandpeace.html"
+title=getTitle(url)
+nameList=getName(url2)
+if title==None:
+	print("Title could not be found")
+else:
+	print(title)	
+if nameList==None:
+	print("Namelist could not be found")
+else:
+	for name in nameList:
+		print(name.get_text())					
+
