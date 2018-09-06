b35ab41e62e4c2b0682ed73e4d9c92cc66e7782b
LCZforever <794295908@qq.com>
2018-09-06 23:39:54 +0800

    kill some mistake

--- beautifulsoup_learn.py
+++ beautifulsoup_learn.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-filename = 'html_file.txt'
-with open(filename, 'r', encoding='utf-8') as f:   #打开存放html的txt文件
+htmlfile1 = 'html_file.txt'  #用html_file.txt正常点
+htmlfile2 = 'html_file2.txt'
+soupfile1 = 'soupfile.txt'
+soupfile2 = 'soupfile2.txt'
+
+with open(htmlfile, 'r', encoding='utf-8') as f:   #打开存放html的txt文件
     contens=f.read()              #读取文件
 
 soup = BeautifulSoup(contens)   #创建beautifulsoup对象
@@ -12,6 +16,7 @@ print(soup.prettify())             #打印观察一下
 
 print(soup.title.string)
 print("hello")
+print(soup.body.parent)
 
 
 
@@ -30,10 +35,10 @@ print("hello")
 
 
 
-
-def save_as_txt(soup):    #存被beautifulsoup解析过的html.soup是beautifulsoup对象
-     filename_write = 'soupfile.txt'    #弄个txt文件来存beautifulsoup处理后的html文件
+def save_as_txt(soup,file):    #存被beautifulsoup解析过的html.soup是beautifulsoup对象
      content = soup.prettify()            #弄到字符串里去
      print(type(content))              #打印观察一下
-     with open(filename_write,'w', encoding='utf-8') as f:   #打开存beautifulsoup对象的txt文件
+     with open(file,'w', encoding='utf-8') as f:   #打开存beautifulsoup对象的txt文件
          f.write(content)                #写入文件
+
+save_as_txt(soup,soupfile1)
--- savehtmlasTXT.py
+++ savehtmlasTXT.py
@@ -1,7 +1,8 @@
 import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-filename = 'html_file2.txt'
+filename = 'html_file.txt'
+filename2 = 'html_file2.txt'
 url1="http://en.wikipedia.org/wiki/Comparison_of_text_editors" #网址1
 url2="http://www.pythonscraping.com/pages/page3.html" #网址2
 def gethtml(url):     #打开网址并返回html信息
@@ -15,6 +16,9 @@ print(content)
 #print(type(content))
 print('\n\n\n')
 print(content.decode())
+
 html = content.decode()
 with open(filename,'w', encoding='utf-8') as f:
     f.write(str(content))
+with open(filename2,'w', encoding='utf-8') as f:
+    f.write(html)
